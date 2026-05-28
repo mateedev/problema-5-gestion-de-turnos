921d366aefbf83a80ec8344b566dfd086190b8d7
Mateo Appio <mateedev@gmail.com>
2026-05-28 15:27:18 -0300

    Funcion para crear o sobreescribir el archivo lista

--- sistema.py
+++ sistema.py
@@ -46,3 +46,26 @@ def desempaquetar_paciente(registro_b):
         'telefono': telefono,
         'prioridad': prioridad
       }
+
+
+
+def crear_archivo_pacientes(ruta, lista_pacientes):
+      """
+      Crea un archivo binario y escribe secuencialmente la lista de pacientes.
+      
+      Precondicion: ruta es un str valido; lista_pacientes es una lista de diccionarios con las claves requeridas para empaquetar_paciente.
+      
+      Postcondición: escribe los registros empaquetados en el archivo. Y crea o sobrescribe un archivo.
+      """
+      
+      with open(ruta, "wb") as archivo:  # wb lo que hace es crear o sobreescribir
+            for p in lista_pacientes:
+                  registro = empaquetar_paciente(
+                        p["dni"],
+                        p["apellido"],
+                        p["nombre"],
+                        p["telefono"],
+                        p["prioridad"]
+                  )
+                  archivo.write(registro)
+                  
